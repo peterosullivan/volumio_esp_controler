afc5ce9935769569dea42d92c6fc868020203878
Peter O'Sullivan <peter@peterosullivan.net>
2019-09-09 08:06:09 +1000

    Try using interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,30 @@
 #define outputB 5 //D2
 #define buttonPin 0 //D3
 
-int buttonState = 0;
+//int buttonState = 0;
+//Bounce button;
 int counter = 0; 
 int aState;
 int aLastState;
-boolean play = true;
-Bounce button;
+
 ESP8266WiFiMulti WiFiMulti;
 WiFiClient  client;
 HTTPClient http;
 
 String volumioEndpoint = "http://volumio.local/api/v1/commands/?cmd=";
+
+ICACHE_RAM_ATTR void pin_ISR()
+{
+  //static unsigned long last_interrupt_time = 0;
+  //unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  //if (interrupt_time - last_interrupt_time > 200)
+  //{
+    //http.begin(client, volumioEndpoint + "toggle");
+    //http.GET();
+    Serial.println("toggle play/pause");
+  //}
+}
 
 void setup(){
   Serial.begin (115200);
@@ -38,31 +51,24 @@ void setup(){
 
   // Reads the initial state of the outputA
   aLastState = digitalRead(outputA);
-  button.attach(buttonPin, INPUT_PULLUP);
-}
+  //button.attach(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT);
 
-inline void togglePlay(){
-  button.update();
-  if(button.fell()){
-    http.begin(client, volumioEndpoint + "toggle");
-    http.GET();
-    Serial.println("toggle play/pause");
-  }
+  attachInterrupt(0, pin_ISR, FALLING);
 }
 
 void loop() { 
-  togglePlay();
   aState = digitalRead(outputA); // Reads the "current" state of the outputA
   // If the previous and the current state of the outputA are different, that means a Pulse has occured
   if (aState != aLastState){     
     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
     if (digitalRead(outputB) != aState) { 
       counter ++;
-      http.begin(client, volumioEndpoint + "volume&volume=plus");
+      http.begin(client, volumioEndpoint + "volume&volume=minus");
       http.GET();
     } else {
       counter --;
-      http.begin(client, volumioEndpoint + "volume&volume=minus");
+      http.begin(client, volumioEndpoint + "volume&volume=plus");
       http.GET();
     }
     Serial.print("Position: ");
